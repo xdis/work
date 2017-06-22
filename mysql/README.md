39ceef44b8836357a98dd37b31c1e2c6c3381312
cmk02 <408824338@qq.com>
2017-06-22 14:55:24 +0800

    循环DECLARE cur_1 CURSOR再运行操作

--- mysql/README.md
+++ mysql/README.md
@@ -18,21 +18,23 @@
 				- select cast(‘125e342.83’ as signed) as clm1		//转换正型
 	- 批量查询表的记录数量
 		- use information_schema
-```
-SELECT CONCAT(
-    'select "', 
-    TABLE_name, 
-    '", count(*) from ', 
-    TABLE_SCHEMA, 
-    '.',
-    TABLE_name,
-    ' union all'
-) FROM TABLES 
-WHERE TABLE_SCHEMA='数据库名';
-```
+
+			```
+			SELECT CONCAT(
+			    'select "', 
+			    TABLE_name, 
+			    '", count(*) from ', 
+			    TABLE_SCHEMA, 
+			    '.',
+			    TABLE_name,
+			    ' union all'
+			) FROM TABLES 
+			WHERE TABLE_SCHEMA='数据库名';
+			```
 	- 1=1妙用
 		- AND IF(_which_day !='0000-00-00', dj_checklist.which_date = _which_day, '1=1') 
 	- 动态执行sql
+
 		```
 		BEGIN
 		 SET @sql = concat('select * from ', $tableName);	 
@@ -46,6 +48,8 @@ WHERE TABLE_SCHEMA='数据库名';
 		```
 		left join (SELECT Amount,CreateTime,LoanId FROM invest WHERE IsValid = 1 AND invest.TransferFlag != 3) AS invest 
 		```
+	- function函数
+		- [循环DECLARE cur_1 CURSOR再运行操作](fn/function/function_循环DECLARE cur_1 CURSOR再运行操作.sql)
 	- 找回密码
 	  - 方法1
 		  - 在 [mysqld]  加入 skip_grant_tables
---
+++ mysql/fn/function/function_循环DECLARE cur_1 CURSOR再运行操作.sql
@@ -0,0 +1,51 @@
+DELIMITER $$
+
+USE `qian100`$$
+
+DROP FUNCTION IF EXISTS `activity_kuaiche_jifen`$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `activity_kuaiche_jifen`(
+      _loan_id INTEGER                	-- 投标的ID  
+) RETURNS VARCHAR(4) CHARSET utf8
+BEGIN
+  /* 理财快车积分兑换  第一步 */
+  DECLARE _IsRecash INTEGER;
+  DECLARE _LoanTerm INTEGER;     -- 标的多少期 
+  DECLARE _invest_id INTEGER;
+  DECLARE _invest_amount DECIMAL(18,2);
+  DECLARE _member_id INTEGER;
+  DECLARE activity_kuaiche_jifen_insert INTEGER;
+  
+  -- 游标定义
+  DECLARE finished INT DEFAULT 0;     
+     DECLARE cur_1 CURSOR 
+       FOR 
+         --  SELECT invest_id,invest_amount,member_id from investkuaiche where loan_id = _loan_id and STATUS = 0;
+           SELECT investkuaiche.invest_id,investkuaiche.invest_amount,investkuaiche.member_id 
+		FROM investkuaiche INNER JOIN invest ON invest.id = investkuaiche.invest_id 
+		WHERE investkuaiche.loan_id = _loan_id AND investkuaiche.STATUS = 0 AND invest.IsValid = 1; 
+           
+      DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;   
+      
+   -- 查询理财快车投标的记录
+    SELECT LoanTerm INTO _LoanTerm FROM loan WHERE id = _loan_id;
+      -- 游标开始                    
+       OPEN cur_1;  
+       curl:REPEAT  
+	FETCH cur_1 INTO _invest_id,_invest_amount,_member_id;  
+	
+	 IF NOT finished THEN 
+	 
+	    SELECT activity_kuaiche_jifen_insert(_member_id,_invest_id,_LoanTerm,_invest_amount,_loan_id) INTO activity_kuaiche_jifen_insert; 
+             -- INSERT INTO sysstoragelog (log_name,log_step) VALUES('积分兑换',CONCAT('activity_kuaiche_jifen_insert(',_member_id,',',_invest_id,',',_LoanTerm,',',_invest_amount,',',_loan_id,')'));
+        END IF;         
+          UNTIL finished  
+        -- 循环结束 
+        END REPEAT curl;  
+       -- 关闭游标 
+       CLOSE cur_1;
+          
+     RETURN '1';
+    END$$
+
+DELIMITER ;
